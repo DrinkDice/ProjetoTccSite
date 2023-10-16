344c1bbee016d7dc520a8a56f475d0226a9d4608
DrinkDice <brunnosoares5555@gmail.com>
2023-10-16 10:34:20 -0300

    atulização dos settings

--- hiremusic/settings.py
+++ hiremusic/settings.py
@@ -46,9 +46,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.whiteNoiseMiddleware"
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
