f1d663f68ed93d9ce674f9136709948fe894e977
DrinkDice <brunnosoares5555@gmail.com>
2023-10-16 15:54:17 -0300

    atulização dos settings

--- hmusic/apps.py
+++ hmusic/apps.py
@@ -4,3 +4,15 @@ from django.apps import AppConfig
 class HmusicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hmusic'
+
+    def ready(self):
+        from .models import Usuario
+        import os
+
+        email = os.getenv("EMAIL_ADMIN")
+        senha = os.getenv("SENHA_ADMIN")
+
+        usuarios = Usuario.objects.filter(email=email)
+        if not usuarios:
+            Usuario.objects.create_superuser(username="admin", email=email, password=senha,
+                                             is_active=True, is_staff=True)
